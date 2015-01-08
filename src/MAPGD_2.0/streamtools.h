cabaea21ce5111ccecd0e8333b679e294ce864b2
Matthew <matthew.s.ackerman@gmail.com>
2015-01-07 19:49:55 -0500

    v2.0, added a new command

--- src/MAPGD_2.0/streamtools.h
+++ src/MAPGD_2.0/streamtools.h
@@ -7,46 +7,6 @@
 #include <sstream>
 
 /*another tutorail code snipit*/
-class opipe
-{
-public:
-    opipe(std::ostream & dst, std::ostream & src)
-        : src(src), sbuf(src.rdbuf(dst.rdbuf())) {}
-//    ~opipe2() { src.rdbuf(sbuf); }
-private:
-    std::ostream & src;
-    std::streambuf * const sbuf;
-};
-
-class ipipe
-{
-public:
-    ipipe(std::istream & dst, std::istream & src)
-        : src(src), sbuf(src.rdbuf(dst.rdbuf())) {}
-//    ~ipipe2() { src.rdbuf(sbuf); }
-private:
-    std::istream & src;
-    std::streambuf * const sbuf;
-};
-
-/*Thanks Viet for this code!*/
-struct opiped {
-    opiped(std::streambuf * buf, std::ostream & os)
-    :os(os), old_buf(os.rdbuf(buf)) { }
-    ~opiped() { os.rdbuf(old_buf); }
-
-    std::ostream& os;
-    std::streambuf * old_buf;
-};
-
-struct ipiped {
-    ipiped(std::streambuf * buf, std::istream & is)
-    :is(is), old_buf(is.rdbuf(buf)) { }
-    ~ipiped() { is.rdbuf(old_buf); }
-
-    std::istream& is;
-    std::streambuf * old_buf;
-};
 
 std::vector<std::string> split(std::string s, char delim);
 #endif
