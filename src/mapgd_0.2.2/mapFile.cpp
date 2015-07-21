7d058ba67538cbc53e6bb1ab08a50a41021c7a54
Matthew <matthew.s.ackerman@gmail.com>
2015-07-21 12:58:17 -0400

    Branched old code.

--- src/mapgd_0.2.2/mapFile.cpp
+++
@@ -1,5 +0,0 @@
-#include "mapFile.h"
-
-/*Design notes: This library is probably poorly thought out. More or less everything works through the 'profile'. However, 
-  * @returns 0 iff successful
-**/
