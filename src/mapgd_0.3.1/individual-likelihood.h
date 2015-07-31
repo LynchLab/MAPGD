4b1df7b5349161914736f26388944ca15dde8e1e
Matthew <matthew.s.ackerman@gmail.com>
2015-07-31 09:35:14 -0400

    .c .h

--- src/mapgd_0.3.1/individual-likelihood.h
+++ src/mapgd_0.3.1/individual-likelihood.h
@@ -15,9 +15,9 @@
 
 
 count_t init_params(Locus&, allele_stat&, const float_t&);										//!< Uses a huristic method for guessing good priors.
-count_t maximize_analytical(Locus&, allele_stat&, models&, std::vector <float_t>&, const float_t&, const count_t&);	//!< Returns values assuming that all reads are identical.
-count_t maximize_grid      (Locus&, allele_stat&, models&, std::vector <float_t>&, const float_t&, const count_t&);	//!< Uses a grid based search to maximize the likelihoood function. 
-count_t maximize_newton    (Locus&, allele_stat&, models&, std::vector <float_t>&, const float_t&, const count_t&);	//!< Uses a newton-raphson method to find the maximum likelihoood priors. This function is still numerically unstable. 
+count_t maximize_analytical(Locus&, allele_stat&, models&, std::vector <float_t>&, const float_t&, const size_t&);	//!< Returns values assuming that all reads are identical.
+count_t maximize_grid      (Locus&, allele_stat&, models&, std::vector <float_t>&, const float_t&, const size_t&);	//!< Uses a grid based search to maximize the likelihoood function. 
+count_t maximize_newton    (Locus&, allele_stat&, models&, std::vector <float_t>&, const float_t&, const size_t&);	//!< Uses a newton-raphson method to find the maximum likelihoood priors. This function is still numerically unstable. 
 float_t loglikelihood(const Locus&, const allele_stat&, models&, const count_t&);	//!< Returns the loglikelihood of a set of quartets given the priors allele_stat
 
 #endif
