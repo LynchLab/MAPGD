367259bb56a60b95c1b835caaee4c4eb41c511df
Matthew <matthew.s.ackerman@gmail.com>
2016-04-15 13:20:04 -0400

    automatic commit

--- search/all_2.js
+++ search/all_2.js
@@ -9,6 +9,7 @@ var searchData=
   ['commands',['commands',['../classEnvironment.html#a16ee1034e66b0d38c769edaea70d1097',1,'Environment']]],
   ['concatenated',['concatenated',['../classBase__file.html#accfc822429915de3f8836174744c6dc9',1,'Base_file']]],
   ['concatenated_5f',['concatenated_',['../classBase__file.html#aaa35644e5e2636edb25d734e234f7f92',1,'Base_file']]],
+  ['cov',['cov',['../classPooled__data.html#a45500d7dd8c2fef416df625025109b36',1,'Pooled_data']]],
   ['coverage',['coverage',['../classAllele.html#ac91659409e1a0ed57def249efbb2a6b1',1,'Allele']]],
   ['cumulative_5fsize_5f',['cumulative_size_',['../classFile__index.html#a564f6ad8c74f9d92e3193fc6c63df346',1,'File_index']]]
 ];
