002f2aa85f4e261eb1a29e8d873436e0d128f9c6
Matthew <matthew.s.ackerman@gmail.com>
2016-08-04 15:07:05 -0400

    automatic commit

--- search/all_3.js
+++ search/all_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['data_20conversion',['Data conversion',['../Conversion.html',1,'']]],
   ['d_5f',['D_',['../classLinkage.html#a640487a528da5c759494b1c2bd11d4e5',1,'Linkage']]],
   ['data',['Data',['../classData.html',1,'']]],
   ['data_5ffile',['Data_file',['../classData__file.html',1,'']]],
@@ -9,6 +10,5 @@ var searchData=
   ['delim',['delim',['../classAllele.html#a82dce8235e786b0764b9d5f316cad16d',1,'Allele::delim()'],['../classLinkage.html#acf28b9d979eea452b62a2f490e01cf42',1,'Linkage::delim()'],['../classLog.html#a26e1fb03a0855d89a2cc87818db018e5',1,'Log::delim()'],['../classPooled__data.html#a6ec6ec3afb0cd570becdd6c658813c97',1,'Pooled_data::delim()'],['../classRelatedness.html#a834395682469c5ea224d9296e8fe0b5d',1,'Relatedness::delim()'],['../classSample__gof.html#a966bd1bea84d2cec5c84fa1de0a2e3c5',1,'Sample_gof::delim()'],['../classSample__name.html#a04192fa8a6e14a5bcef98a758771394d',1,'Sample_name::delim()']]],
   ['delim_5fcolumn_5f',['delim_column_',['../classBase__file.html#a2da466fc2101aaaa88f15961a8c0af7a',1,'Base_file']]],
   ['description',['description',['../classEnvironment.html#a6208c7eef3dc8ba7edff579ac746c2eb',1,'Environment']]],
-  ['dp',['DP',['../classVcf__data.html#a443c10e692d1718e7f42a62ca1061785',1,'Vcf_data']]],
-  ['data_20conversion_20_7bconversion_7d',['Data conversion {Conversion}',['../md_mapgd_0_84_data_conversion_README.html',1,'']]]
+  ['dp',['DP',['../classVcf__data.html#a443c10e692d1718e7f42a62ca1061785',1,'Vcf_data']]]
 ];
--- search/pages_1.js
+++ search/pages_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['data_20types',['Data types',['../DataTypes.html',1,'']]],
-  ['data_20conversion_20_7bconversion_7d',['Data conversion {Conversion}',['../md_mapgd_0_84_data_conversion_README.html',1,'']]]
+  ['data_20conversion',['Data conversion',['../Conversion.html',1,'']]],
+  ['data_20types',['Data types',['../DataTypes.html',1,'']]]
 ];
