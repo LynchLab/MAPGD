8c2fec61907a43bb7562b680bbb26ebb40e1bdca
Matthew Ackerman <ma18@sanger.ac.uk>
2019-01-30 15:39:34 +0000

    Getting old versions of htslib working...

--- src/mapgd_0.4/data_conversion/vcf_file.cc
+++ src/mapgd_0.4/data_conversion/vcf_file.cc
@@ -4,6 +4,36 @@
 
 #define MIN_GL	1.798486e-111
 
+#ifndef BCF_HDR_SYNC
+int bcf_hdr_sync(bcf_hdr_t *h)
+{
+    int i;
+    for (i = 0; i < 3; i++)
+    {
+        vdict_t *d = (vdict_t*)h->dict[i];
+        khint_t k;
+        if ( h->n[i] < kh_size(d) )
+        {
+            bcf_idpair_t *new_idpair;
+            new_idpair = (bcf_idpair_t*) realloc(h->id[i], kh_size(d)*sizeof(bcf_idpair_t));
+            if (!new_idpair) return -1;
+            h->n[i] = kh_size(d);
+            h->id[i] = new_idpair;
+        }
+        for (k=kh_begin(d); k<kh_end(d); k++)
+        {
+            if (!kh_exist(d,k)) continue;
+            h->id[i][kh_val(d,k).id].key = kh_key(d,k);
+            h->id[i][kh_val(d,k).id].val = &kh_val(d,k);
+        }
+    }
+    h->dirty = 0;
+    return 0;
+}
+#endif
+
+
+
 void call_minor(const  Genotype &gtl, int32_t *calls)
 {
 	if (gtl.MM>gtl.Mm)
